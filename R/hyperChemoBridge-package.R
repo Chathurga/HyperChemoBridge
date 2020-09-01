a32b5b31ad41bda469567817e274cb1bd8a2abe2
Bryan <hanson@depauw.edu>
2020-09-01 09:49:28 -0400

    create package doc

---
+++ R/hyperChemoBridge-package.R
@@ -0,0 +1,11 @@
+#' Bridge between hyperSpec & ChemoSpec packages
+#'
+#' Functions for converting between hyperSpec and ChemoSpec (Spectra) objects
+#' as used by the respective packages.
+#'
+#' @name hyperChemoBridge-package
+#' @aliases hyperChemoBridge-package hyperChemoBridge
+#' @docType package
+#' @author Conor McManus
+#' @keywords package
+NULL
